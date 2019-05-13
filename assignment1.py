4cdc620ab4215a0eb6115eb0fd3a954b7f6757a1
lisa <lisa.schneider@stud.fh-campuswien.ac.at>
2019-05-13 18:32:10 +0200

    fix total_average_coverage

--- assignment1.py
+++ assignment1.py
@@ -11,7 +11,7 @@ __author__ = 'Lisa Schneider'
 
 
 class Assignment1:
-    
+
     def __init__(self):
         ## Your gene of interest
         self.gene = 'CRYZL1'
@@ -25,17 +25,22 @@ class Assignment1:
         self.reads = self.get_all_reads()
 
     def download_gene_coordinates(self, genome_reference, file_name):
+        '''
+        :param genome_reference:
+        :param file_name:
+        :return:
+        '''
         ## TODO concept
         
         print("Connecting to UCSC to fetch data")
         
-        ## Open connection
+        # Open connection
         cnx = mysql.connector.connect(host='genome-mysql.cse.ucsc.edu', user='genomep', passwd='password', db=genome_reference)
         
-        ## Get cursor
+        # Get cursor
         cursor = cnx.cursor()
         
-        ## Build query fields
+        # Build query fields
         query_fields = ["refGene.name2",
                         "refGene.name",
                         "refGene.chrom",
@@ -46,10 +51,10 @@ class Assignment1:
                         "refGene.exonStarts",
                         "refGene.exonEnds"]
         
-        ## Build query
+        # Build query
         query = "SELECT DISTINCT {} from refGene WHERE name2 = '{}'".format(",".join(query_fields), self.gene)
         
-        ## Execute query
+        # Execute query
         cursor.execute(query)
         
         ## Write to file
@@ -74,14 +79,9 @@ class Assignment1:
         return gene_list
 
     def get_coordinates_of_gene(self):
-        gene = self.gene_list[0]
         chromosome = self.gene_list[2]
         start = self.gene_list[3]
         stop = self.gene_list[4]
-        print(f"Coordinates of gene {gene}: \n"
-              f"\tchromosome - {chromosome} \n"
-              f"\tstart - {start} \n"
-              f"\tstop - {stop}")
         return [chromosome, int(start), int(stop)]
         
     def get_gene_symbol(self):
@@ -93,11 +93,11 @@ class Assignment1:
         print("\nSam Header:")
         for k, v in self.alignment_file.header["HD"].items():
             if k == "SO":
-                print(f"\tSO (Sorting order of Alignments): {v}")
+                print(f"\tSO - Sorting order of Alignments: {v}")
             if k == "VN":
-                print(f"\tVN (Format version): {v}")
+                print(f"\tVN - Format version: {v}")
             if k == "GO":
-                print(f"\tGO: (Grouping of alignments): {v}")
+                print(f"\tGO - Grouping of alignments: {v} \n")
 
     def get_all_reads(self):
         reads = list(self.alignment_file.fetch(self.chromosome,
@@ -107,38 +107,41 @@ class Assignment1:
 
     def get_properly_paired_reads_of_gene(self):
         proper_reads = len([i for i in self.reads if i.is_proper_pair])
-        print(f"Number of properly paired reads: {proper_reads}")
+        print(f"Properly paired reads: {proper_reads}")
         
     def get_gene_reads_with_indels(self):
-        # Cigar = Compact Idiosyncratic Gapped Alignment Report
-        # cigar operation 1 = insertion
-        # cigar operation 2 = deletion
-        rd_indel = []
+        indel_list = []
         for i in self.reads:
             if not i.is_unmapped:
                 cig = i.cigartuples
                 for (operation, length) in cig:
-                    if (operation == 1) or (operation == 2):
-                        rd_indel.append(i)
-        reads_indel = len(rd_indel)
-        print(f"Reads with indels: {reads_indel}")
+                    if (operation == 1) or (operation == 2): # cigar operation 1 = insertion, 2 = deletion
+                        indel_list.append(i)
+        indel_number = len(indel_list)
+        print(f"Reads with indels: {indel_number}")
         
     def calculate_total_average_coverage(self):
-        # todo to slow
-        print("Starting to calculate chromosome coverage")
-        chromosome_length = [i["LN"] for i in self.alignment_file.header["SQ"] if i["SN"] == self.chromosome][0]
-        print("chromosome length calculated")
-        coverage = self.alignment_file.count_coverage(self.chromosome, start=0, stop=chromosome_length)
-        print("coverage calculated")
-        chromosome_average_coverage = round(numpy.mean(coverage), 2)
-        print("average coverage calculated")
-        print(f"{chromosome_average_coverage} \n")
+        total_coverage = []
+        for pileup in self.alignment_file.pileup(self.chromosome):
+            total_coverage.append(pileup.n)
+        average_total_coverage = sum(total_coverage) / float(len(total_coverage))
+        print(f"Average total coverage: {average_total_coverage} \n")
+
+
+        # # todo to slow
+        # print("Starting to calculate chromosome coverage")
+        # chromosome_length = [i["LN"] for i in self.alignment_file.header["SQ"] if i["SN"] == self.chromosome][0]
+        # print("chromosome length calculated")
+        # coverage = self.alignment_file.count_coverage(self.chromosome, start=0, stop=chromosome_length)
+        # print("coverage calculated")
+        # chromosome_average_coverage = round(numpy.mean(coverage), 2)
+        # print("average coverage calculated")
+        # print(f"{chromosome_average_coverage} \n")
 
     def calculate_gene_average_coverage(self):
-        chromosome = self.gene_coordinates[0]
-        coverage = self.alignment_file.count_coverage(chromosome,
-                                                      start=self.gene_coordinates[1],
-                                                      stop=self.gene_coordinates[2])
+        coverage = self.alignment_file.count_coverage(self.chromosome,
+                                                      start=self.start,
+                                                      stop=self.stop)
         average_gene_coverage = round(numpy.mean(coverage), 2)
         print(f"Average gene coverage: {average_gene_coverage}")
         
@@ -154,36 +157,34 @@ class Assignment1:
         print(f"Genomic Region:\n"
               f"\tChromosome: {self.chromosome}\n"
               f"\tStart: {self.start}\n"
-              f"\tStop: {self.stop}")
+              f"\tStop: {self.stop} \n")
         
     def get_number_of_exons(self):
         exons = self.gene_list[6]
-        print(f"exons : {exons}")
-    
-    
-    def print_summary(self):
-        # self.read_gene_coordinates()
-        # self.get_gene_symbol()
-        # self.get_number_of_exons()
-        # self.get_properly_paired_reads_of_gene()
-        # self.get_number_mapped_reads()
-        # self.get_sam_header()
-        self.calculate_total_average_coverage()
-        # self.calculate_gene_average_coverage()
-        # self.get_region_of_gene()
-        # self.get_gene_reads_with_indels()
+        print(f"Exons : {exons}")
 
-        print("Print all results here")
-    
-    
+    def print_summary(self):
+        self.read_gene_coordinates()
+        # print gene coordinates
+        print(f"Coordinates of gene {self.gene}: \n"
+              f"\tChromosome - {self.chromosome} \n"
+              f"\tStart - {self.start} \n"
+              f"\tStop - {self.stop} \n")
+        self.get_gene_symbol()
+        self.get_number_of_exons()
+        self.get_properly_paired_reads_of_gene()
+        self.get_number_mapped_reads()
+        self.get_sam_header()
+        self.calculate_total_average_coverage()
+        self.calculate_gene_average_coverage()
+        self.get_region_of_gene()
+        self.get_gene_reads_with_indels()
+
+
 def main():
-    print("Assignment 1")
     assignment1 = Assignment1()
     assignment1.print_summary()
-    
-    
-    print("Done with assignment 1")
-    
-        
+
+
 if __name__ == '__main__':
     main()
